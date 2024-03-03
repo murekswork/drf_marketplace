ceda01b909cb13580a6cddf44d64a1011eb8e2a8
murekswork <murekswork@gmail.com>
2024-03-03 15:41:44 +0300

    filter backend. validation if user create order with his own product

--- backend/order/views.py
+++ backend/order/views.py
@@ -1,8 +1,10 @@
 import datetime
 
+import django_filters
 from api.mixins import UserQuerySetMixin
 from django.db.models import Q
 from rest_framework import generics, status
+from rest_framework.exceptions import APIException
 from rest_framework.response import Response
 
 from .mixins import OrderServiceFabricMixin
@@ -11,19 +13,26 @@ from .serialziers import OrderSerializer
 
 
 class OrderListCreateAPIView(UserQuerySetMixin, generics.ListCreateAPIView):
-
     serializer_class = OrderSerializer
     queryset = Order.objects.all()
     allow_staff_view = False
+    filter_backends = [django_filters.rest_framework.DjangoFilterBackend]
+    filterset_fields = ['payment_status']
 
     def get_queryset(self):
         qs = super().get_queryset()
         qs = qs.filter(Q(lifetime__gte=datetime.datetime.now()) | Q(payment_status=True))
         return qs
 
+    def perform_create(self, serializer):
+        product = serializer.validated_data['product_pk']
+        if product.user != self.request.user:
+            super().perform_create(serializer)
+        else:
+            raise APIException('You can not buy your own product!')
+
 
 class OrderPayAPIView(UserQuerySetMixin, OrderServiceFabricMixin, generics.RetrieveAPIView):
-
     lookup_field = 'pk'
     lookup_url_kwarg = 'pk'
     serializer_class = OrderSerializer
