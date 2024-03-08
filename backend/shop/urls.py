f0d7c1ccf8dc50458255d6cba49e782cd5694e41
murekswork <murekswork@gmail.com>
2024-03-08 22:38:44 +0300

    now shop ShopDetailAPIView based on shop slug field

--- backend/shop/urls.py
+++ backend/shop/urls.py
@@ -3,9 +3,9 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('<int:pk>/', views.ShopDetailAPIView.as_view(), name='shop-detail'),
+    path('<str:slug>/', views.ShopDetailAPIView.as_view(), name='shop-detail'),
     path('', views.ShopListAPIView.as_view(), name='shop-list'),
-    path('upload_products/csv', views.UploadCSVProductsAPIView.as_view(), name='upload-csv'),
-    path('upload_products/results/<int:pk>', views.UploadCSVProductsAPIView.as_view(), name='upload-detail'),
-    path('upload_products/', views.UploadsAPIVIew.as_view(), name='upload-list')
+    path('<str:slug>/upload_products/csv', views.UploadCSVProductsAPIView.as_view(), name='upload-csv'),
+    path('<str:slug>/upload_products/results/<int:pk>', views.UploadCSVProductsAPIView.as_view(), name='upload-detail'),
+    path('<str:slug>/upload_products/', views.UploadsAPIVIew.as_view(), name='upload-list')
 ]
