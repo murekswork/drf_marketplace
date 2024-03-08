32c82f58ac341693ea84b60ef711662f5ed193b2
murekswork <murekswork@gmail.com>
2024-03-08 22:31:01 +0300

    slug creating logic modified in save method of shop model

--- backend/shop/models.py
+++ backend/shop/models.py
@@ -1,9 +1,7 @@
 from django.contrib.auth import get_user_model
 from django.contrib.auth.models import Permission
 from django.db import models
-
-from products.models import Product
-from articles.models import Article
+from django.utils.text import slugify
 
 
 class ShopQuerySet(models.QuerySet):
@@ -20,7 +18,7 @@ class ShopStaffGroup(models.Model):
     group_name = models.CharField(max_length=120, default='Group name')
     permissions = models.ManyToManyField(Permission, blank=True, limit_choices_to={
         'codename__in': ['add_product', 'delete_product', 'delete_sale', 'change_product', 'change_sale', 'add_sale'],
-        })
+    })
 
     def __str__(self):
         return f'{self.group_name}'
@@ -31,11 +29,9 @@ class ShopManager(models.Model):
     shop = models.ForeignKey('Shop', on_delete=models.CASCADE, null=True)
     title = models.CharField(max_length=120, null='Role title')
     group = models.ForeignKey('ShopStaffGroup', on_delete=models.SET_NULL, null=True)
-    # permission = models.ForeignKey(Permission
 
 
 class ProductUpload(models.Model):
-
     user = models.ForeignKey(get_user_model(), on_delete=models.SET_NULL, null=True)
     created_at = models.DateTimeField(auto_now_add=True)
     products_count = models.IntegerField(default=0)
@@ -53,8 +49,20 @@ class Shop(models.Model):
     description = models.TextField(max_length=500, default='Shop Description')
     active = models.BooleanField(default=True)
     managers = models.ManyToManyField(through=ShopManager, to=get_user_model())
+    slug = models.SlugField(unique=True, max_length=500, editable=False)
 
     objects = ShopObjectManager()
+
+    def save(self, *args, **kwargs):
+        if not self.slug:
+            base_slug = slugify(self.title)
+            slug = base_slug
+            counter = 1
+            while Shop.objects.filter(slug=slug).exists():
+                slug = f"{base_slug}-{counter}"
+                counter += 1
+            self.slug = slug
+        super(Shop, self).save(*args, **kwargs)
 
     def is_active(self):
         return self.active
