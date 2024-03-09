206a5917f0d49df0c0115601340da34301babb0b
murekswork <murekswork@gmail.com>
2024-03-10 02:55:19 +0300

    added file response with uploads report

--- backend/shop/views.py
+++ backend/shop/views.py
@@ -3,6 +3,7 @@ import time
 from api.authentication import TokenAuthentication
 from api.mixins import UserQuerySetMixin
 from celery_app import get_upload_logs
+from django.http import FileResponse
 from rest_framework import status
 from rest_framework.authentication import SessionAuthentication
 from rest_framework.generics import ListAPIView, RetrieveAPIView, get_object_or_404
@@ -22,13 +23,14 @@ from shop.throttles import OncePerHourThrottleForPost
 
 class ShopDetailAPIView(RetrieveAPIView):
     serializer_class = ShopWithProductsSerializer
-    queryset = Shop.objects.all()
+    queryset = Shop.objects.all().prefetch_related('shopmanager_set', 'shopmanager_set__group',
+                                                   'shopmanager_set__user', 'products', 'products__sales')
     lookup_field = 'slug'
 
 
 class ShopListAPIView(ListAPIView):
     serializer_class = ShopSerializer
-    queryset = Shop.objects.all()
+    queryset = Shop.objects.all().prefetch_related('shopmanager_set', 'shopmanager_set__group', 'shopmanager_set__user')
 
 
 class UploadCSVProductsAPIView(
@@ -48,14 +50,17 @@ class UploadCSVProductsAPIView(
 
         self.check_permissions(request)
         file = request.data.get('file', None)
+
+        # TODO: delegate ProductCSVUploader service actions to celery !
         if file is not None:
             upload = ProductUpload.objects.create(user=self.request.user,
                                                   file_name=f'{time.time()}_{self.request.user}')
             upload.save()
             service = ProductCSVUploader(source=file, shop=shop)
             service.upload()
-            tasks = service.get_tasks()
-            get_upload_logs.delay(tasks=tasks, upload_id=str(upload.id))
+            service.save_tasks()
+            tasks_filename = service.get_tasks_filename()
+            get_upload_logs.delay(tasks_file_name=tasks_filename, upload_id=str(upload.id))
             msg = 'Started to uploading provided csv file'
         else:
             msg = 'No file provided'
@@ -67,20 +72,24 @@ class UploadCSVProductsAPIView(
             return Response('Its not your shop!', status=status.HTTP_403_FORBIDDEN)
         obj = get_object_or_404(ProductUpload, pk=pk)
         try:
-            with open(f'backend/tasks_data/{obj.file_name}.txt') as f:
-                logs = f.read()
-                return Response(logs, status=200)
+            response = FileResponse(open(f'backend/tasks_data/{obj.file_name}.csv', 'rb'), as_attachment=True,
+                                    filename='upload_results.csv')
+            return response
         except Exception as e:
             return Response(f'Your products are not uploaded yet! {e}', status=status.HTTP_425_TOO_EARLY)
 
 
 class UploadsAPIVIew(
     UserQuerySetMixin,
-    ListAPIView
+    APIView
 ):
     permission_classes = (IsAuthenticated,)
     serializer_class = ProductUploadSerializer
     queryset = ProductUpload.objects.all()
     authentication_classes = (TokenAuthentication, SessionAuthentication,)
 
-# Create your views here.
+    # TODO: rewrite UploadProduct model with shop as user field and then filter by shop slug ( OR ID!!! )
+    def get(self, request, slug, *args, **kwargs):
+        queryset = ProductUpload.objects.filter(user=request.user)
+        serializer = self.serializer_class(queryset, many=True, context={'request': request, 'slug': slug})
+        return Response(serializer.data)
