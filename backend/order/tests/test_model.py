e690117a8025a27867d561c84116ee8c4120c2bd
murekswork <murekswork@gmail.com>
2024-03-11 23:31:16 +0300

    tests

--- backend/order/tests/test_model.py
+++ backend/order/tests/test_model.py
@@ -1,7 +1,7 @@
 from django.contrib.auth import get_user_model
 from django.test import TestCase
 from order.models import Order
-from order.services.order_service import OrderServiceFabric
+from order.services.order_service import OrderServiceFactory
 from products.models import Product
 from shop.models import Shop
 from wallet.models import Wallet
@@ -38,6 +38,34 @@ class OrderModelTestCase(TestCase):
     def test_payment_status_field_when_payed(self):
         self.order_product.quantity = 50
         self.order_product.save()
-        service = OrderServiceFabric.get_order_service(self.order)
+        service = OrderServiceFactory.get_order_service(self.order)
         service.pay_order()
         self.assertEqual(self.order.payment_status, True)
+
+
+class OrderModelSaveMethodTestCase(TestCase):
+
+    def setUp(self):
+        self.user = get_user_model().objects.create_user(username='test', email='test@mail.com')
+        self.shop = Shop.objects.create(user=self.user, title='title', description='description')
+        self.product = Product.objects.create(shop=self.shop, title='title', public=True, content='content',
+                                              quantity=5)
+        self.order = Order.objects.create(product=self.product, count=3, user=self.user)
+
+    def test_order_model_when_create_not_paid_then_product_sales_count_not_increases(self):
+        self.assertTrue(self.product.sales_count == 0)
+
+    def test_order_model_when_create_and_paid_then_product_sales_count_increases(self):
+        self.order.payment_status = True
+        self.order.save()
+        self.product.refresh_from_db()
+        self.assertTrue(self.product.sales_count == 1)
+
+    def test_order_model_when_create_and_paid_second_then_product_sales_count_increases(self):
+        self.order2 = Order.objects.create(product=self.product, count=3, user=self.user)
+        self.order.payment_status = True
+        self.order.save()
+        self.order2.payment_status = True
+        self.order2.save()
+        self.product.refresh_from_db()
+        self.assertTrue(self.product.sales_count == 2)
