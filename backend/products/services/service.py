3f665213f9ba413a731d266c286930b51348224d
murekswork <murekswork@gmail.com>
2024-03-11 23:31:49 +0300

    added sale validation service

--- backend/products/services/service.py
+++ backend/products/services/service.py
@@ -1,4 +1,6 @@
-from products.models import Product
+import datetime
+
+from products.models import Product, Sale
 from utils.validate_service import BadWordsValidator
 
 
@@ -13,3 +15,14 @@ class ProductBadWordsValidateService(BadWordsValidator):
         self.obj.public = False
         self.obj.save()
         return self.obj
+
+
+class SaleValidationService:
+
+    def __init__(self, sale: Sale):
+        self.sale = sale
+
+    def validate_sale_expired(self) -> dict[str, bool | str]:
+        if self.sale.end_date > datetime.datetime.now():
+            return {'success': True}
+        return {'success': False, 'message': 'Sale is expired!'}
