22aa824622057fe88ac6e974607c058eb3630ba5
murekswork <murekswork@gmail.com>
2024-03-13 01:45:09 +0300

    simplified post in ProductListCreateAPIView

--- backend/products/views.py
+++ backend/products/views.py
@@ -11,8 +11,8 @@ from rest_framework.generics import (
     get_object_or_404,
 )
 from rest_framework.response import Response
-from shop.models import Shop
-from shop.permissions import ProductShopStaffPermission
+from shop.models import Shop, ShopPermissions
+from shop.permissions import ProductShopStaffPermission, ShopStaffPermissionsMixin
 
 from .models import Product
 from .serializers import (
@@ -32,7 +32,8 @@ class ProductFilter(django_filters.FilterSet):
 
 
 class ProductListCreateAPIView(
-    ListCreateAPIView
+    ListCreateAPIView,
+    ShopStaffPermissionsMixin
 ):
     queryset = (Product.objects.filter(public=True).prefetch_related('articles', 'sales', 'orders').select_related())
     serializer_class = ProductSerializer
@@ -43,16 +44,12 @@ class ProductListCreateAPIView(
     search_fields = ('title', 'content')
 
     def post(self, request, *args, **kwargs):
-        # TODO: REWRITE THIS MAYBE
         shop = get_object_or_404(Shop, id=int(request.data.get('shop', '0')))
-        if request.user.is_authenticated:
-            user_role = shop.managers.through.objects.filter(user=request.user)
-            if user_role.exists():
-                user_role = user_role.all()[0]
-                if user_role.has_permission('create_shop_product'):
-                    return super().post(request, *args, **kwargs)
 
-        return Response({'You dont have permission to upload products in this shop'}, status=status.HTTP_403_FORBIDDEN)
+        if self.has_shop_permission(request, shop, ShopPermissions.CREATE_PRODUCT):
+            return super().post(request, *args, **kwargs)
+
+        return Response({'You dont have permission to upload products!'}, status=status.HTTP_403_FORBIDDEN)
 
     def get(self, request, *args, **kwargs):
         return super().get(request, *args, **kwargs)
@@ -111,31 +108,3 @@ class ProductUpdateAPIView(
         instance = serializer.save()
         if not instance.content:
             instance.content = 'blank content'
-
-# @api_view(['GET', 'POST'])
-# def alt_product_view(request, pk=None, *args, **kwargs):
-#
-#     method = request.method
-#
-#     if method == 'GET':
-#         if pk is not None:
-#             obj = get_object_or_404(Product, pk=pk)
-#             data = ProductSerializer(obj, many=False).data
-#             return Response(data)
-#
-#         qs = Product.objects.all()
-#         data = ProductSerializer(qs, many=True).data
-#         return Response(data, status=status.HTTP_200_OK)
-#
-#     if method == 'POST':
-#
-#         serializer = ProductSerializer(data=request.data)
-#         if serializer.is_valid(raise_exception=True):
-#             title = serializer.validated_data.get('title')
-#             content = serializer.validated_data.get('content', None)
-#             if content is None:
-#                 serializer.save(content=title)
-#             serializer.save(content=content)
-#             return Response(serializer.data)
-#
-#         return Response({"error": "Not allowed", "message": "not good data"}, status=status.HTTP_400_BAD_REQUEST)
