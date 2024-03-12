51be3d486b07273b66c3fdb349c470745ea38e66
murekswork <murekswork@gmail.com>
2024-03-13 01:45:39 +0300

    refactor

--- backend/search/views.py
+++ backend/search/views.py
@@ -1,5 +1,4 @@
 from api.mixins import UserQuerySetMixin
-from django.db.models import Avg, Count, Q
 from products.models import Product
 from products.serializers import ProductSerializer
 from rest_framework import generics
@@ -17,12 +16,5 @@ class SearchListView(UserQuerySetMixin, generics.ListAPIView):
         q = self.request.GET.get('q')
         result = Product.objects.none()
         if q is not None:
-            user = None
-            if self.request.user.is_authenticated:
-                user = self.request.user
-            result = qs.search(q, user=user)
-
-        return (result.prefetch_related('articles', 'sales', 'orders')
-                      .select_related()
-                      .annotate(mark=Avg('articles__mark', default=5),
-                                sales_count=Count('orders', filter=Q(orders__payment_status=True))))
+            result = qs.search(q)
+        return result.prefetch_related('articles', 'sales', 'orders', 'shop')
