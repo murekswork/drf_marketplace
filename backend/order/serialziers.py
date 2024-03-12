58fb45d74958618ab76e02db1e237bdac4ebe421
murekswork <murekswork@gmail.com>
2024-03-13 01:42:14 +0300

    tiny refactoring

--- backend/order/serialziers.py
+++ backend/order/serialziers.py
@@ -1,5 +1,6 @@
 from api.serializers import UserSerializer
 from products.models import Product
+from products.serializers import ProductInlineSerializer
 from rest_framework import serializers
 from rest_framework.reverse import reverse
 
@@ -9,8 +10,8 @@ from .validators import positive_integer_validator
 
 class OrderSerializer(serializers.ModelSerializer):
     user = UserSerializer(read_only=True)
-    product = serializers.SerializerMethodField(read_only=True)
-    product_pk = serializers.PrimaryKeyRelatedField(queryset=Product.objects.all(), write_only=True)
+    choose_product = serializers.PrimaryKeyRelatedField(write_only=True, queryset=Product.objects.filter(public=True))
+    product = ProductInlineSerializer(read_only=True)
     amount = serializers.SerializerMethodField()
     count = serializers.IntegerField(validators=[positive_integer_validator])
     payment_status = serializers.BooleanField(read_only=True)
@@ -21,28 +22,27 @@ class OrderSerializer(serializers.ModelSerializer):
         model = Order
         fields = '__all__'
 
-    def get_payment_url(self, object):
-        if object.payment_status is True:
+    def get_payment_url(self, obj):
+        if obj.payment_status is True:
             return ''
-        return reverse('order-payment', kwargs={'pk': object.pk}, request=self.context.get('request'))
+        return reverse('order-payment', kwargs={'pk': obj.pk}, request=self.context.get('request'))
 
-    def get_product(self, object):
+    def get_product_url(self, obj):
         request = self.context.get('request')
         return reverse(
-            viewname='product-detail', request=request, kwargs={'pk': object.product.pk}
+            viewname='product-detail', request=request, kwargs={'pk': obj.product.pk}
         )
 
-    def get_amount(self, object):
-        sale = object.product.sales.all()
-        if sale and object.payment_status is not True:
-            return str(
-                (float(object.product.price) - (float(object.product.price) * 0.01 * float(sale[0].size))) * object.count)
-        return object.amount
+    def get_amount(self, obj):
+        sale = obj.product.sales.all()
+        if sale and obj.payment_status is not True:
+            return (float(obj.product.price) - (float(obj.product.price) * 0.01 * float(sale[0].size))) * obj.count
+        return obj.amount
 
     def create(self, validated_data):
         request = self.context.get('request')
         user = request.user
-        product_pk = validated_data.pop('product_pk')
+        product_pk = validated_data.pop('choose_product')
         product = Product.objects.get(pk=product_pk.pk)
         count = validated_data.pop('count')
         amount = product.price * count
