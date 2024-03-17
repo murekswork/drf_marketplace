e3aa7285ec617bcc4646c663c503e8a10daa4c67
murekswork <murekswork@gmail.com>
2024-03-18 01:44:26 +0300

    kafka tg adapter

---
+++ backend/delivery/services/delivery_service.py
@@ -0,0 +1,36 @@
+import logging
+from abc import ABC, abstractmethod
+
+from delivery.models import Delivery
+
+
+class DeliveryFabricServiceABC(ABC):
+
+    @abstractmethod
+    def create_delivery(self, delivery_data) -> Delivery:
+        raise NotImplementedError
+
+    @abstractmethod
+    def send_delivery(self, delivery: Delivery):
+        raise NotImplementedError
+
+    @abstractmethod
+    def receive_delivery(self):
+        raise NotImplementedError
+
+
+class DeliveryFromTgAdapter:
+
+    def update_delivery_status_from_telegram(self, delivery_dict: dict) -> Delivery:
+        try:
+            cour_id = delivery_dict.pop('courier')
+            delivery_dict['courier_id'] = cour_id
+            d = Delivery.objects.filter(id=delivery_dict['id']).first()
+            if d:
+                for key, value in delivery_dict.items():
+                    setattr(d, key, value)
+                logging.info('(SUCCESS) Updated delivery in database!')
+                d.save()
+            return d
+        except Exception as e:
+            logging.error(f'Could not update delivery in db coz of {e}', exc_info=True)
