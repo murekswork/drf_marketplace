c3cb1505c2e39b24fc9955e375a56f88e4a96c76
murekswork <murekswork@gmail.com>
2024-03-19 01:47:10 +0300

    now when DjangoDeliveryReceiver receives completed or canceled delivery - update delivery and courier objects in db

--- backend/delivery/kafka_/receiver.py
+++ backend/delivery/kafka_/receiver.py
@@ -2,7 +2,9 @@ import json
 import logging
 
 from bot.kafka_common.receiver import KafkaReceiver
-from delivery.models import Delivery
+from courier.kafka_.sender import send_courier_profile
+from courier.services import CourierDeliveryService
+from delivery.services.delivery_service import DeliveryFromTgAdapter
 
 
 class DjangoDeliveryReceiver(KafkaReceiver):
@@ -31,22 +33,20 @@ class DjangoDeliveryReceiver(KafkaReceiver):
 
             logging.info(f'(SUCCESS) Got incoming delivery {msg} from django !')
             try:
+
                 d = msg['delivery']
-                self.update_delivery_in_db(d)
+
+                # Updates delivery status in database
+                d_service = DeliveryFromTgAdapter()
+                d_db = d_service.update_delivery_status_from_telegram(d)
+
+                # Updates courier status in database
+                c_service = CourierDeliveryService()
+                c_db = c_service.close_delivery(d_db)
+
+                # Sends back updated courier data
+                send_courier_profile({'id': c_db.id})
 
             except Exception as e:
                 logging.error(f'Could not unparse incoming delivery msg to dataclass! {e}')
         self.consumer.close()
-
-    def update_delivery_in_db(self, delivery_dict: dict):
-        try:
-            cour_id = delivery_dict.pop('courier')
-            delivery_dict['courier_id'] = cour_id
-            d = Delivery.objects.filter(id=delivery_dict['id']).first()
-            if d:
-                for key, value in delivery_dict.items():
-                    setattr(d, key, value)
-                logging.info('(SUCCESS) Updated delivery in database!')
-                d.save()
-        except Exception as e:
-            logging.error(f'Could not update delivery in database! coz of {e}')
