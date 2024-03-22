74767de7f086a12ae979ee3efefee7d13ccb9640
murekswork <murekswork@gmail.com>
2024-03-22 23:32:16 +0300

    created handler to mark delivery as picked up

--- backend/bot/handlers/handlers.py
+++ backend/bot/handlers/handlers.py
@@ -42,7 +42,7 @@ async def courier_stop_carrying(update: Update, context: CallbackContext):
 
 
 async def track_location_handler(update: Update, context: CallbackContext):
-    msg = update.message
+    msg = update.edited_message
     user = msg.chat
     try:
         service = CourierService()
@@ -54,28 +54,45 @@ async def track_location_handler(update: Update, context: CallbackContext):
 
 async def send_delivery_info_msg(context: CallbackContext, chat_id, delivery: Delivery):
     await context.bot.send_message(chat_id=chat_id, text=f'{delivery}')
-    await context.bot.send_location(chat_id=chat_id, latitude=delivery.latitude, longitude=delivery.longitude)
+    await send_delivery_pickup_point_msg(
+        context,
+        chat_id,
+        delivery.consumer_latitude,
+        delivery.consumer_longitude
+    )
 
 
-async def distribute_deliveries_periodic_task(context: CallbackContext):
-    service = DeliveryService()
-    deliveries_ = service.start_delivering()
-    logging.warning('Starting deliveries')
+async def picked_up_delivery_handler(update: Update, context: CallbackContext):
+    try:
+        courier_id = update.message.chat.id
+        service = DeliveryService()
+        delivery = await service.picked_up_delivery(courier_id)
 
-    if deliveries is not None:
-        async for i in deliveries_:
-            logger.info(f'Got delivery {i} for delivering')
-            if i['success'] is True:
-                await send_delivery_info_msg(context, chat_id=i['courier'].id, delivery=i['delivery'])
-            else:
-                logger.warning('No free couriers!')
-    else:
-        logger.warning('Distribution does not started because of not free couriers!')
+        await context.bot.send_location(
+            chat_id=courier_id,
+            latitude=delivery.consumer_latitude,
+            longitude=delivery.consumer_longitude
+        )
+        await context.bot.send_message(
+            chat_id=courier_id,
+            parse_mode='HTML',
+            text='Nice job! Now you should bring picked up good you take to client in location below')
+
+    except Exception as e:
+        logger.error(f'Could not receiver pickup point msg coz of, {e}', exc_info=True)
 
 
-async def job_check_deliveries(update, context: CallbackContext):
-    job_queue = context.job_queue
-    job_queue.run_repeating(distribute_deliveries_periodic_task, interval=5, first=0)
+async def send_delivery_pickup_point_msg(context: CallbackContext, chat_id, lat, lon):
+    await context.bot.send_location(
+        chat_id=chat_id,
+        latitude=lat,
+        longitude=lon
+    )
+    await context.bot.send_message(
+        chat_id=chat_id,
+        parse_mode='HTML',
+        text='You should pick up goods on point under this msg'
+    )
 
 
 async def show_couriers_delivery(update: Update, context: CallbackContext):
@@ -88,7 +105,7 @@ async def show_couriers_delivery(update: Update, context: CallbackContext):
 async def close_delivery(update: Update, context: CallbackContext, status: int):
     cour_id = update.message.chat.id
     service = CourierService()
-    delivery = service.close_delivery(cour_id, status)
+    delivery = await service.close_delivery(cour_id, status)
     if delivery:
         await update.message.reply_text(f'Delivery closed with status {status}!')
         await update.message.reply_text(f'Your score will be updated soon! {couriers.get(cour_id, None)}')
