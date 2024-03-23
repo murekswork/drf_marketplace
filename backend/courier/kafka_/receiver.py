6002b6a082f21b006aaaecb44a825a85e13277f8
murekswork <84355451+murekswork@users.noreply.github.com>
2024-03-24 02:10:21 +0300

    Update receiver.py
    unnecessary exception handling del

--- backend/courier/kafka_/receiver.py
+++ backend/courier/kafka_/receiver.py
@@ -11,11 +11,8 @@ class CourierLocationReceiver(KafkaReceiver):
 
     def __init__(self):
         super().__init__()
-        try:
-            self.location_tracker = LocationTracker()
-        except Exception as e:
-            self.logger.error(f'(ERROR) GOT an exception while initializing Location Tracker {e}', exc_info=True)
-
+        self.location_tracker = LocationTracker()
+        
     def post_consume_action(self, msg: str):
         msg_dict = json.loads(msg)
         self.location_tracker.set_location(courier_id=msg_dict['courier_id'], location=msg_dict['location'])
