f6710afb5d3a53d2113380ef9be5602f70a97f2a
murekswork <murekswork@gmail.com>
2024-03-24 22:48:09 +0300

    refactor

--- backend/courier/kafka_/sender.py
+++ backend/courier/kafka_/sender.py
@@ -1,5 +1,3 @@
-import logging
-
 from bot.kafka_common.receiver import SingletonMixin
 from bot.kafka_common.sender import KafkaSender
 from courier.models import Courier
@@ -7,16 +5,12 @@ from django.core.serializers import serialize
 
 
 def send_courier_profile_from_django_to_telegram(courier_dict: dict):
-    try:
-        courier_db = Courier.objects.filter(id=courier_dict['id']).first()
-        if courier_db is None:
-            courier_db = Courier.objects.create(**courier_dict)
-        serialized_courier = serialize('json', [courier_db])
-        producer = CourierProfileProducer()
-        producer.send(msg=serialized_courier)
-
-    except Exception as e:
-        logging.error(f'Could not send courier profile from Django to Tg, coz of {e}')
+    courier_db = Courier.objects.filter(id=courier_dict['id']).first()
+    if courier_db is None:
+        courier_db = Courier.objects.create(**courier_dict)
+    serialized_courier = serialize('json', [courier_db])
+    producer = CourierProfileProducer()
+    producer.send(msg=serialized_courier)
 
 
 class CourierProfileProducer(KafkaSender, SingletonMixin):
--- backend/delivery/kafka_/sender.py
+++ backend/delivery/kafka_/sender.py
@@ -1,6 +1,3 @@
-import json
-import logging
-
 from bot.kafka_common.receiver import SingletonMixin
 from bot.kafka_common.sender import KafkaSender
 from delivery.models import Delivery
@@ -8,17 +5,9 @@ from django.core.serializers import serialize
 
 
 def send_delivery_to_tg(delivery_orm: Delivery):
-    try:
-        # delivery_dto = model_to_dataclass_converter(delivery_orm, DeliveryDTO)
-        serialized_delivery = serialize('json', [delivery_orm])
-        msg = {
-            'delivery_id': delivery_orm.id,
-            'delivery': serialized_delivery
-        }
-        sender = DjangoDeliverySender()
-        sender.send(json.dumps(msg, default=str))
-    except Exception as e:
-        logging.error(f'Could not send delivery to tg coz of {e}')
+    serialized_delivery = serialize('json', [delivery_orm])
+    producer = DjangoDeliverySender()
+    producer.send(serialized_delivery)
 
 
 class DjangoDeliverySender(KafkaSender, SingletonMixin):
