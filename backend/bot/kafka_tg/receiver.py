120c07af1af1e07566181ee8c800daf11c16db4c
murekswork <murekswork@gmail.com>
2024-03-24 04:19:52 +0300

    code refactoring

--- backend/bot/kafka_tg/receiver.py
+++ backend/bot/kafka_tg/receiver.py
@@ -1,26 +1,34 @@
 import json
+from dataclasses import dataclass
 
 from kafka_common.receiver import KafkaReceiver
 from schemas.schemas import Courier, Delivery, couriers, deliveries
 
 
-def dict_to_dataclass(dict_: dict, dataclass):
+def dict_to_dataclass(dict_: dict, dataclass_: type):
     """Function to convert dict to dataclass by same fields"""
     same_fields = {
-        field: dict_[field] for field in dict_ if field in dataclass.__annotations__
+        field: dict_[field] for field in dict_ if field in dataclass_.__annotations__
     }
     return dataclass(**same_fields)
+
+
+def consume_django_model_to_dataclass(serialized_model: str, dataclass_: type):
+    """Function takes serialized django model and dataclass type and converts it to dataclass object"""
+    deserialized_msg = json.loads(serialized_model)[0]
+    model_dict = deserialized_msg['fields']
+    model_dict['id'] = deserialized_msg.pop('pk')
+    courier_dataclass = dict_to_dataclass(model_dict, dataclass_)
+    return courier_dataclass
 
 
 class CourierProfileReceiver(KafkaReceiver):
     _topic = 'courier_profile'
 
-    def post_consume_action(self, msg: str):
+    def post_consume_action(self, msg: str) -> None:
         """Method deserializes incoming message to courier and adds courier profile to line"""
-        deserialized_msg = json.loads(msg)[0]
-        courier_dict = deserialized_msg['fields']
-        courier_dict['id'] = deserialized_msg.pop('pk')
-        courier_dataclass = dict_to_dataclass(courier_dict, Courier)
+        courier_dataclass = consume_django_model_to_dataclass(msg, Courier)
+        courier_dict = courier_dataclass.__dict__
 
         if courier_dict['id'] not in couriers:
             couriers[courier_dict['id']] = courier_dataclass
@@ -33,10 +41,7 @@ class CourierProfileReceiver(KafkaReceiver):
 class TgDeliveryReceiver(KafkaReceiver):
     _topic = 'to_deliver'
 
-    def post_consume_action(self, msg: str):
+    def post_consume_action(self, msg: str) -> None:
         """Method deserializes incoming message in delivery and adds delivery to queue"""
-        msg_dict = json.loads(msg)
-        delivery_dict = json.loads(msg_dict['delivery'])[0]['fields']
-        delivery_dict['id'] = msg_dict['delivery_id']
-        dataklass = dict_to_dataclass(delivery_dict, Delivery)
-        deliveries[delivery_dict['id']] = dataklass
+        delivery_dataclass = consume_django_model_to_dataclass(msg, Delivery)
+        deliveries[delivery_dataclass.id] = delivery_dataclass
