227b38668c09f26a05006cdf4fb0726af19a664d
murekswork <murekswork@gmail.com>
2024-03-26 01:18:24 +0300

    replaced funcs

--- backend/bot/kafka_tg/receiver.py
+++ backend/bot/kafka_tg/receiver.py
@@ -1,32 +1,13 @@
-import json
-from dataclasses import dataclass
-
 from kafka_common.receiver import KafkaReceiver
 from schemas.schemas import Courier, Delivery, couriers, deliveries
-
-
-def dict_to_dataclass(dict_: dict, dataclass_: type):
-    """Function to convert dict to dataclass by same fields"""
-    same_fields = {
-        field: dict_[field] for field in dict_ if field in dataclass_.__annotations__
-    }
-    return dataclass(**same_fields)
-
-
-def consume_django_model_to_dataclass(serialized_model: str, dataclass_: type):
-    """Function takes serialized django model and dataclass type and converts it to dataclass object"""
-    deserialized_msg = json.loads(serialized_model)[0]
-    model_dict = deserialized_msg['fields']
-    model_dict['id'] = deserialized_msg.pop('pk')
-    courier_dataclass = dict_to_dataclass(model_dict, dataclass_)
-    return courier_dataclass
+from utils import consume_django_model_to_dataclass
 
 
 class CourierProfileReceiver(KafkaReceiver):
     _topic = 'courier_profile'
 
     def post_consume_action(self, msg: str) -> None:
-        """Method deserializes incoming message to courier and adds courier profile to line"""
+        """Method to deserialize incoming message from to courier and adds courier profile to line"""
         courier_dataclass = consume_django_model_to_dataclass(msg, Courier)
         courier_dict = courier_dataclass.__dict__
 
@@ -42,6 +23,6 @@ class TgDeliveryReceiver(KafkaReceiver):
     _topic = 'to_deliver'
 
     def post_consume_action(self, msg: str) -> None:
-        """Method deserializes incoming message in delivery and adds delivery to queue"""
+        """Method to deserialize incoming message in delivery and adds delivery to queue"""
         delivery_dataclass = consume_django_model_to_dataclass(msg, Delivery)
         deliveries[delivery_dataclass.id] = delivery_dataclass
