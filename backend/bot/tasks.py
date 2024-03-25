5e6fdf21d6e7f3846476b64152b4b0e50adcf43f
murekswork <murekswork@gmail.com>
2024-03-26 01:11:14 +0300

    added task for notifications sending

--- backend/bot/tasks.py
+++ backend/bot/tasks.py
@@ -1,7 +1,11 @@
-from handlers.delivery_handlers import send_delivery_info_msg
+from handlers.delivery_handlers import (
+    delivery_taking_late_notification,
+    send_delivery_info_msg,
+)
 from keyboards import CourierReplyMarkups
 from logging_.logger import logger
 from services.delivery_service import DeliveryService
+from services.notification_service import NotificationService
 from telegram import Update
 from telegram.ext import CallbackContext
 
@@ -22,7 +26,19 @@ async def distribute_deliveries_periodic_task(context: CallbackContext):
         logger.warning('Distribution does not started because of not free couriers!')
 
 
+async def delivery_notification_periodic_task(context: CallbackContext):
+    service = NotificationService()
+    to_notify_deliveries = await service.distribute_notifications()
+    for delivery in to_notify_deliveries:
+        await delivery_taking_late_notification(context, delivery)
+
+
 async def job_check_deliveries(update: Update, context: CallbackContext):
     await update.message.reply_text(text='Select action', reply_markup=CourierReplyMarkups.COURIER_MAIN_MARKUP)
     job_queue = context.job_queue
     job_queue.run_repeating(distribute_deliveries_periodic_task, interval=5, first=0)
+
+
+async def job_notify_courier(update: Update, context: CallbackContext):
+    job_queue = context.job_queue
+    job_queue.run_repeating(delivery_notification_periodic_task, interval=5, first=0)
